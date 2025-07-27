521b679d2c8a1500aea1ec10b79c5a6c157f556d
qwaqd <qwaq.dev@gmail.com>
2025-07-27 12:06:36 +0500

    for deploy on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // иначе будут ошибки с Image-компонентом
+  },
+  basePath: "/dlp-ui-audata", 
+  eslint: {
+    ignoreDuringBuilds: true, 
+  },
 };
 
 export default nextConfig;
