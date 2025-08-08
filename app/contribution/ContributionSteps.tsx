0989b95865c6207a2552948f39c58b85a73c33d9
qwaqd <qwaq.dev@gmail.com>
2025-08-08 05:44:45 +0500

    end of this day

--- app/contribution/ContributionSteps.tsx
+++ app/contribution/ContributionSteps.tsx
@@ -42,7 +42,7 @@ export const contributionSteps: Step[] = [
   {
     id: 3,
     title: "Request Validation",
-    description: "Submitting job to validation nodes",
+    description: "Checking for audio valid",
     icon: <Server className="h-5 w-5" />,
     statusMessage: "Checking is your audio valid...",
   },
